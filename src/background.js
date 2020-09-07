04bfa39d8062d351959aef09ed2df6c64947677c
_y_s <ys0115sp@gmail.com>
2020-09-07 22:59:57 +0900

    v0.7b7; delete src-chrome

--- src/background.js
+++ src/background.js
@@ -2,182 +2,208 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- (function () {
-	'use strict';
-	let locus = false, touch = false;
-	const ports = [];
-	let enabled;
-	let blacklist = [];
-	
-	// load storage
-	browser.storage.local.get(['config', 'gesture', 'blacklist']).then(v => {
-		locus = v && v.config.locus || false;
-		touch = v && v.config.touch || false;
+//@ts-check
+let locus = false, touch = false;
+/** @type {browser.runtime.Port[]} */
+const ports = [];
+/** @type {Map<string,number>} */
+let enabled;
+let blacklist = [];
 
-		let basic = 2047, extra = 0;
-		const gesture = v && v.gesture;
-		if (gesture) {
-			basic = gesture.basic;
-			extra = gesture.extra;
-		} else {
-			// for first use: open options page, discard other tabs 
-			browser.runtime.openOptionsPage();
-			if ('discard' in browser.tabs) {
-				browser.tabs.query({ discarded: false }).then(tabs => {
-					browser.tabs.discard(tabs.map(tab => tab.id));
-				});
-			}
-			// for Android
-			if (!('windows' in browser)) {
-				basic = 511;
-			}
-			browser.storage.local.set({
-				version: browser.runtime.getManifest().version,
-				gesture: { basic: basic, extra: extra },
+// load storage
+browser.storage.local.get(['config', 'gesture', 'blacklist']).then(v => {
+	locus = v && v.config.locus || false;
+	touch = v && v.config.touch || false;
+
+	let basic = 2047, extra = 0;
+	const gesture = v && v.gesture;
+	if (gesture) {
+		basic = gesture.basic;
+		extra = gesture.extra;
+	} else {
+		// for first use: open options page, discard other tabs 
+		browser.runtime.openOptionsPage();
+		if ('discard' in browser.tabs) {
+			browser.tabs.query({ discarded: false }).then(tabs => {
+				browser.tabs.discard(tabs.map(tab => tab.id));
 			});
 		}
-		enabled = new Map([
-			['left', basic & 1],
-			['right', basic & 2],
-			['up,left', basic & 4],
-			['up,down', basic & 8],
-			['up', basic & 16],
-			['down', basic & 32],
-			['down,up', basic & 64],
-			['wheel', basic & 128],
-			['down,right', basic & 256],
-			['down,left', basic & 512],
-			['up,right', basic & 1024],
-			['right,up', extra & 1],
-			['wheel-skip', extra & 2],
-		]);
-		blacklist = (v && v.blacklist || [
-			'^(?:about|chrome|file|jar|moz-extension|resource|view-source)',
-			'\\\.(?:pdf|svg)$',
-			'^https?://(?:accounts-static|addons|content)\\\.cdn\\\.mozilla\\\.net/',
-			'^https?://(?:addons|discovery\\\.addons|input|install|support)\\\.mozilla\\\.org/',
-			'^https?://(?:accounts|testpilot)\\\.firefox\\\.com/',
-			'^https?://(?:api|oauth|profile)\\\.accounts\\\.firefox\\\.com/',
-			'^https?://sync\\\.services\\\.mozilla\\\.com/',
-		]).map(s => new RegExp(s));
-	});
+		// for Android
+		if (!('windows' in browser)) {
+			basic = 511;
+		}
+		browser.storage.local.set({
+			version: browser.runtime.getManifest().version,
+			gesture: { basic: basic, extra: extra },
+		});
+	}
+	enabled = new Map([
+		['left', basic & 1],
+		['right', basic & 2],
+		['up,left', basic & 4],
+		['up,down', basic & 8],
+		['up', basic & 16],
+		['down', basic & 32],
+		['down,up', basic & 64],
+		['wheel', basic & 128],
+		['down,right', basic & 256],
+		['down,left', basic & 512],
+		['up,right', basic & 1024],
+		['right,up', extra & 1],
+		['wheel-skip', extra & 2],
+	]);
+	blacklist = (v && v.blacklist || [
+		'^(?:about|chrome|file|jar|moz-extension|resource|view-source)',
+		'\\\.(?:pdf|svg)$',
+		'^https?://(?:accounts-static|addons|content)\\\.cdn\\\.mozilla\\\.net/',
+		'^https?://(?:addons|discovery\\\.addons|input|install|support)\\\.mozilla\\\.org/',
+		'^https?://(?:accounts|testpilot)\\\.firefox\\\.com/',
+		'^https?://(?:api|oauth|profile)\\\.accounts\\\.firefox\\\.com/',
+		'^https?://sync\\\.services\\\.mozilla\\\.com/',
+	]).map(s => new RegExp(s));
+});
 
-	browser.runtime.onConnect.addListener(p => {
-		if (!blacklist.some(re => re.test(p.sender.url))) {
-			const id = p.sender.tab.id;
-			ports[id] = p;
-			p.onMessage.addListener(onmessage.bind(p));
-			if (locus) {
-				browser.tabs.executeScript(id, {
-					file: 'locus.js',
-					runAt: 'document_end',
-				});
+browser.runtime.onConnect.addListener(p => {
+	if (!blacklist.some(re => re.test(p.sender.url))) {
+		const id = p.sender.tab.id;
+		ports[id] = p;
+		p.onMessage.addListener(onMessage.bind(p));
+		if (locus) {
+			browser.tabs.executeScript(id, {
+				file: 'locus.js',
+				runAt: 'document_end',
+			});
+		}
+		if (touch) {
+			browser.tabs.executeScript(id, {
+				allFrames: true,
+				file: 'touch.js',
+				runAt: 'document_end',
+			});
+		}
+	}
+});
+
+// main
+/** @type {string[]} */
+let gestures = [];
+const isUrl = url => typeof url === 'string' && url.startsWith('http');
+/** @type {Map<string, (url?: string) => Promise<void>>} */
+const func = new Map([
+	['left', async function () {
+		browser.tabs.goBack();
+	}],
+	['right', async function () {
+		browser.tabs.goForward();
+	}],
+	['up', async function () {
+		browser.tabs.executeScript({
+			code: 'window.stop();',
+			runAt: 'document_start',
+		});
+	}],
+	['up,left', async function (url) {
+		browser.tabs.update({ url: url.replace(/[^/]*\/?$/, '') });
+	}],
+	['up,down', async function () {
+		browser.tabs.reload({ bypassCache: true });
+	}],
+	['down', async function (url) {
+		browser.tabs.create(isUrl(url) ? { url: url } : {});
+	}],
+	['down,up', async function (url) {
+		const _ = isUrl(url);
+		if (!_ && 'duplicate' in browser.tabs) {
+			browser.tabs.duplicate(this.sender.tab.id);
+		} else {
+			browser.tabs.create({ active: false, url: _ ? url : this.sender.url });
+		}
+	}],
+	['down,right', async function () {
+		browser.tabs.remove(this.sender.tab.id);
+	}],
+	['down,left', async function () {
+		const currentWindow = await browser.windows.getCurrent();
+		browser.windows.update(currentWindow.id, { state: 'minimized' });
+	}],
+	['up,right', async function () {
+		const currentWindow = await browser.windows.getCurrent();
+		browser.windows.update(currentWindow.id, { state: currentWindow.state === 'normal' ? 'maximized' : 'normal' });
+	}],
+	['right,up', async function () {
+		const sessions = await browser.sessions.getRecentlyClosed({ maxResults: 1 });
+		if (sessions.length) {
+			browser.sessions.restore((sessions[0].tab || sessions[0].window).sessionId);
+		}
+	}],
+]);
+
+/**
+ * @this {browser.runtime.Port} port
+ * @param { { direction?: number, execute?: boolean, states?: string[], url?: string } } m message
+ */
+async function onMessage(m) {
+	const port = this;
+	if (enabled.get('wheel') && 'direction' in m) {
+		const tabs = await browser.tabs.query({ currentWindow: true });
+		let idx = port.sender.tab.index + m.direction;
+		let target = tabs[idx];
+		/**
+		 * @param {?browser.runtime.Port} isPort 
+		 */
+		function judgeSkip(isPort) {
+			if (enabled.get('wheel-skip') && !isPort) {
+				idx += m.direction;
+				selectTab();
+			} else {
+				port.postMessage({ func: 'leave' });
+				ports[target.id].postMessage({ func: 'enter' });
 			}
-			if (touch) {
-				browser.tabs.executeScript(id, {
-					allFrames: true,
-					file: 'touch.js',
-					runAt: 'document_end',
+		}
+		/**
+		 * @param {browser.runtime.Port} port 
+		 */
+		function observeConnection(port) {
+			browser.runtime.onConnect.removeListener(observeConnection);
+			judgeSkip(port);
+		}
+		function selectTab() {
+			if (idx < 0) {
+				idx = tabs.length - 1;
+			} else if (idx >= tabs.length) {
+				idx = 0;
+			}
+			target = tabs[idx];
+			if (target.discarded) {
+				browser.runtime.onConnect.addListener(observeConnection);
+				browser.tabs.update(target.id, { active: true });
+			} else {
+				browser.tabs.update(target.id, { active: true }).then(() => {
+					judgeSkip(ports[target.id]);
 				});
 			}
 		}
-	});
-
-	// main
-	let gestures = [];
-	const isUrl = url => typeof url === 'string' && url.startsWith('http');
-	const func = new Map([
-		['down', async function (url) {
-			const opt = isUrl(url) ? { url: url } : {};
-			browser.tabs.create(opt);
-		}],
-		['down,up', async function (url) {
-			const _ = isUrl(url);
-			if (!_ && 'duplicate' in browser.tabs) {
-				browser.tabs.duplicate(this.sender.tab.id);
-			} else {
-				const opt = { active: false, url: _ ? url : this.sender.url };
-				browser.tabs.create(opt);
+		selectTab();
+	} else {
+		if (m.states) {
+			const l = gestures.length;
+			const state = m.states[+(l && m.states[0] === gestures[l-1] && m.states[1] !== gestures[l-1])];
+			if (state && state !== gestures[l-1] && state !== gestures[l-2]) {
+				gestures.push(state);
 			}
-		}],
-		['down,right', async function () {
-			browser.tabs.remove(this.sender.tab.id);
-		}],
-		['down,left', async function () {
-			const currentWindow = await browser.windows.getCurrent();
-			const opt = { state: 'minimized' };
-			browser.windows.update(currentWindow.id, opt);
-		}],
-		['up,right', async function () {
-			const currentWindow = await browser.windows.getCurrent();
-			const opt = { state: currentWindow.state === 'normal' ? 'maximized' : 'normal' };
-			browser.windows.update(currentWindow.id, opt);
-		}],
-		['right,up', async function () {
-			const sessions = await browser.sessions.getRecentlyClosed({ maxResults: 1 });
-			if (sessions.length) {
-				browser.sessions.restore((sessions[0].tab || sessions[0].window).sessionId);
-			}
-		}],
-	]);
-
-	async function onmessage(m) {
-		const port = this;
-		if (enabled.get('wheel') && 'direction' in m) {
-			const tabs = await browser.tabs.query({ currentWindow: true });
-			let idx = port.sender.tab.index + m.direction;
-			let target = tabs[idx];
-			function judgeSkip(isPort) {
-				if (enabled.get('wheel-skip') && !isPort) {
-					idx += m.direction;
-					selectTab();
-				} else {
-					port.postMessage({ func: 'leave' });
-					ports[target.id].postMessage({ func: 'enter' });
-				}
-			}
-			function observeConnection(port) {
-				browser.runtime.onConnect.removeListener(observeConnection);
-				judgeSkip(port);
-			}
-			function selectTab() {
-				if (idx < 0) {
-					idx = tabs.length - 1;
-				} else if (idx >= tabs.length) {
-					idx = 0;
-				}
-				target = tabs[idx];
-				if (target.discarded) {
-					browser.runtime.onConnect.addListener(observeConnection);
-					browser.tabs.update(target.id, { active: true });
-				} else {
-					browser.tabs.update(target.id, { active: true }).then(() => {
-						judgeSkip(ports[target.id]);
+		}
+		if (m.execute) {
+			const gesture = gestures.join();
+			gestures = [];
+			if (enabled.get(gesture)) {
+				if (func.has(gesture)) {
+					func.get(gesture).bind(port)(m.url).catch(e => {
+						port.postMessage({ error: e });
 					});
+				} else {
+					port.postMessage({ func: gesture });
 				}
-			}
-			selectTab();
-		} else {
-			if (m.states) {
-				const l = gestures.length;
-				const state = m.states[(l && m.states[0] === gestures[l-1] && m.states[1] !== gestures[l-1])|0];
-				if (state && state !== gestures[l-1] && state !== gestures[l-2]) {
-					gestures.push(state);
-				}
-			}
-			if (m.execute) {
-				const gesture = gestures.join();
-				if (enabled.get(gesture)) {
-					if (func.has(gesture)) {
-						func.get(gesture).bind(port)(m.url).catch(e => {
-							port.postMessage({ error: e });
-						});
-					} else {
-						port.postMessage({ func: gesture });
-					}
-				}
-				gestures = [];
 			}
 		}
 	}
-})();
+}
--- src/content.js
+++ src/content.js
@@ -2,221 +2,230 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- (function () {
-	'use strict';
-	const EXT_URL = browser.runtime.getURL('/');
-	let first = true;
-	let startX = -1, startY = -1;
+// @ts-check
+const EXT_URL = browser.runtime.getURL('/');
+let first = true;
+let startX = -1, startY = -1;
 
-	function preventDefault(e) {
-		e.preventDefault();
+/**
+ * @param {Event} e 
+ */
+function preventDefault(e) {
+	e.preventDefault();
+}
+
+self.addEventListener('mousedown', e => {
+	if (e.button === 2) {
+		/** @type {?HTMLAnchorElement|HTMLAreaElement} */
+		// @ts-ignore
+		const closest = e.target.closest('a[href],area[href]');
+		top.postMessage({
+			href: closest ? closest.href : null,
+			origin: EXT_URL,
+			screenX: e.screenX,
+			screenY: e.screenY,
+		}, '*');
 	}
+}, false);
+self.addEventListener('contextmenu', e => {
+	if (!first || (startX !== -1 && startX !== e.screenX) || (startY !== -1 && startY !== e.screenY)) {
+		preventDefault(e);
+	}
+}, false);
+
+if (self !== top) {
+	throw null;
+}
+
+// glocal vars
+/** @type { { [key:string]:number } } */
+let vars = {};
+
+// load config
+browser.storage.local.get(['config']).then(/** @param v { { config:{} } } */ v => {
+	const INITIAL = { config: { mm1: 32, mm2: 16, ar: 30, wm: 4 } };
+	if (v.config) {
+		vars = v.config;
+	} else {
+		vars = INITIAL.config;
+		browser.storage.local.set({
+			version: browser.runtime.getManifest().version,
+			config: vars,
+		});
+	}
+	vars.rad = vars.ar / 360 * Math.PI;
+	vars.tan = Math.tan(vars.rad);
+});
+
+// overlay
+const overlay = document.createElement('div');
+overlay.id = EXT_URL.replace(/[/:-]+/g, '-') + 'overlay';
+overlay.hidden = true;
+overlay.style.cssText = 'display:none;position:fixed;height:100%;width:100%;left:0;right:0;z-index:2147483647';
+document.addEventListener('DOMContentLoaded', () => {
+	if (document.body.nodeName.toLowerCase() === 'frameset') {
+		const body = document.createElement('body');
+		document.body = frameset2div(document.body, body);
+		document.documentElement.style.height = '100%';
+	}
+	document.body.appendChild(overlay);
+});
+
+/**
+ * Convert <frameset> element to <div> element
+ * @param {HTMLElement} frameset - <frameset> element
+ * @param {HTMLElement} wrapper - wrapper element
+ */
+function frameset2div(frameset, wrapper = document.createElement('div')) {
+	wrapper.style.display = 'grid';
+	wrapper.style.height = '100%';
+	wrapper.style.width = '100%';
+	wrapper.style.margin = '0';
+	[
+		['cols', 'gridTemplateColumns'],
+		['rows', 'gridTemplateRows']
+	].forEach(([attrName, styleName]) => {
+		const val = frameset.getAttribute(attrName);
+		if (val) {
+			wrapper.style[styleName] = val.split(',').map(v => v.replace(/(\d*)(\D*)/, /** @param p {string[]} */ (...p) => (p[1] || '1') + (p[2] ? p[2] === '*' ? 'fr' : p[2] : 'px'))).join(' ');
+		}
+	});
+	const frameBorder = frameset.getAttribute('frameborder')  || '0';
+	wrapper.dataset.frameBorder = frameBorder;
+	const style = document.createElement('style');
+	style.textContent = `[data-frame-border] iframe{border:${isNaN(+frameBorder) ? 'inherit' : `${frameBorder}px`};height:inherit;width:inherit}`;
+	wrapper.appendChild(style);
 	
-	self.addEventListener('mousedown', e => {
-		if (e.button === 2) {
-			const closest = e.target.closest('a[href],area[href]');
-			top.postMessage({
-				href: closest ? closest.href : null,
-				origin: EXT_URL,
-				screenX: e.screenX,
-				screenY: e.screenY,
-			}, '*');
-		}
-	}, false);
-	self.addEventListener('contextmenu', e => {
-		if (!first || (startX !== -1 && startX !== e.screenX) || (startY !== -1 && startY !== e.screenY)) {
-			preventDefault(e);
-		}
-	}, false);
-
-	if (self !== top) {
-		return false;
-	}
-
-	// glocal vars
-	let vars = {};
-
-	// load config
-	browser.storage.local.get(['config']).then(v => {
-		const INITIAL = { config: { mm1: 32, mm2: 16, ar: 30, wm: 4 } };
-		if (v.config) {
-			vars = v.config;
-		} else {
-			vars = INITIAL.config;
-			browser.storage.local.set({
-				version: browser.runtime.getManifest().version,
-				config: vars,
+	const NEW_ELEMENT_TEMPLATE = {
+		frame: document.createElement('iframe'),
+		frameset: document.createElement('div'),
+	};
+	Array.from(frameset.children).forEach(/** @param {HTMLElement} child */ child => {
+		/** @type {Node} */
+		let newnode;
+		const nodeName = child.nodeName.toLowerCase();
+		if (nodeName in NEW_ELEMENT_TEMPLATE) {
+			/** @type {HTMLElement} */
+			const newelem = NEW_ELEMENT_TEMPLATE[nodeName].cloneNode();
+			Array.from(child.attributes).forEach(attr => {
+				newelem.setAttribute(attr.name, attr.value);
 			});
+			newelem.dataset.frameBorder = frameBorder;
+			newnode = nodeName === 'frameset' ? frameset2div(child, newelem) : newelem;
+		} else {
+			newnode = document.createComment(`<noframes>${child.innerHTML}</noframes>`);
 		}
-		vars.rad = vars.ar / 360 * Math.PI;
-		vars.tan = Math.tan(vars.rad);
+		wrapper.appendChild(newnode);
 	});
+	return wrapper;
+}
 
-	// overlay
-	const overlay = document.createElement('div');
-	overlay.id = EXT_URL.replace(/[/:-]+/g, '-') + 'overlay';
-	overlay.hidden = true;
-	overlay.style.display = 'none';
-	overlay.style.position = 'fixed';
-	overlay.style.height = '100%';
-	overlay.style.width = '100%';
-	overlay.style.left = 0;
-	overlay.style.top = 0;
-	overlay.style.zIndex = 0x7fffffff;
-	document.addEventListener('DOMContentLoaded', () => {
-		if (document.body.nodeName.toLowerCase() === 'frameset') {
-			const body = document.createElement('body');
-			document.body = frameset2div(document.body, body);
-			document.documentElement.style.height = '100%';
-		}
-		document.body.appendChild(overlay);
-	});
-	function frameset2div(frameset, wrapper) {
-		const styleGrid = attr => attr ? attr.split(',').map(v => v.replace(/(\d*)(\D*)/, (_, p1, p2) => (p1 || '1') + (p2 ? p2 === '*' ? 'fr' : p2 : 'px'))).join(' ') : '';
-		if (!wrapper) {
-			wrapper = document.createElement('div');
-		}
-		wrapper.style.display = 'grid';
-		wrapper.style.height = '100%';
-		wrapper.style.width = '100%';
-		wrapper.style.margin = 0;
-		const STYLE_NAMES = ['gridTemplateColumns', 'gridTemplateRows'];
-		['cols', 'rows'].forEach((v, i) => {
-			wrapper.style[STYLE_NAMES[i]] = styleGrid(frameset.getAttribute(v));
-		});
-		const frameborder = frameset.getAttribute('frameborder')  || '';
-		if (frameborder) {
-			wrapper.dataset.frameBorder = frameborder;
-			const style = document.createElement('style');
-			style.textContent = '[data-frame-border] iframe{border:' + (isNaN(frameborder) ? 'inherit}' : frameborder + 'px}');
-			wrapper.appendChild(style);
-		}
-		
-		const NEW_ELEMENT_TEMPLATE = {
-			frame: document.createElement('iframe'),
-			frameset: document.createElement('div'),
-		};
-		Array.from(frameset.children).forEach(child => {
-			let newnode;
-			const nodeName = child.nodeName.toLowerCase();
-			if (nodeName in NEW_ELEMENT_TEMPLATE) {
-				let newelem = NEW_ELEMENT_TEMPLATE[nodeName].cloneNode();
-				Array.from(child.attributes).forEach(attr => {
-					newelem.setAttribute(attr.name, attr.value);
-				});
-				if (frameborder) {
-					newelem.dataset.frameBorder = frameborder;
-				}
-				newnode = nodeName === 'frameset' ? frameset2div(child, newelem) : newelem;
+// main
+const port = browser.runtime.connect();
+let href = '';
+let wheelTimer = 0;
+function initStartCoord() {
+	startX = -1, startY = -1;
+}
+
+/**
+ * Detect and send states
+ * @param {MouseEvent} e 
+ */
+function checkState(e) {
+	const diffX = e.screenX - startX, diffY = e.screenY - startY;
+	const absX = Math.abs(diffX), absY = Math.abs(diffY);
+	const min = first ? vars.mm1 : vars.mm2;
+
+	if (min < absX || min < absY) {			
+		/** @type {?string[]} */
+		let states;
+		if (absX > absY) {
+			if (vars.tan > absY / absX) {
+				states = [diffX < 0 ? 'left' : 'right'];
 			} else {
-				newnode = document.createComment('<noframes>' + child.innerHTML + '</noframes>');
+				states = [null, diffX < 0 ? 'left' : 'right'];
 			}
-			wrapper.appendChild(newnode);
-		});
-		return wrapper;
+		} else if (absY > absX) {
+			if (vars.tan > absX / absY) {
+				states = [diffY < 0 ? 'up' : 'down'];
+			} else {
+				states = [null, diffY < 0 ? 'up' : 'down']
+			}
+		}
+		if (states) {
+			first = false;
+			startX = e.screenX, startY = e.screenY;
+			port.postMessage({ states: states });
+		}
 	}
+}
+/**
+ * @param {WheelEvent} e 
+ */
+function onWheel(e) {
+	e.preventDefault();
+	clearTimeout(wheelTimer);
+	if (startX < 0 || startY < 0) {
+		startX = e.screenX, startY = e.screenY;
+	}
+	startX += e.deltaX, startY += e.deltaY;
+	const diffY = startY - e.screenY;
+	if (vars.wm < Math.abs(diffY)) {
+		startX = e.screenX, startY = e.screenY;
+		port.postMessage({ type: 'wheel', direction: Math.sign(diffY) });
+	}
+	wheelTimer = setTimeout(initStartCoord, 100);
+}
+/**
+ * @param {MouseEvent} e 
+ */
+function onMouseUp(e) {
+	if (e.button === 2) {
+		overlay.hidden = true;
+		overlay.style.display = 'none';
+		port.postMessage({ execute: true, url: href });
+		self.removeEventListener('mousemove', checkState);
+		self.removeEventListener('wheel', onWheel);
+		self.removeEventListener('mouseup', onMouseUp);
+	}
+}
 
-	// main
-	const port = browser.runtime.connect();
-	let href;
-	let wheelTimer;
-	function initStartCoord() {
+self.addEventListener('message', e => {
+	if (e.data.origin === EXT_URL) {
+		overlay.hidden = false;
+		overlay.style.display = 'block';
+		first = true;
+		startX = e.data.screenX, startY = e.data.screenY;
+		href = e.data.href;
+		self.addEventListener('mousemove', checkState, { passive: true });
+		self.addEventListener('wheel', onWheel, { passive: false });
+		self.addEventListener('mouseup', onMouseUp, { passive: true });
+	}
+}, false);
+
+const funcMap = new Map([
+	['enter', () => {
+		overlay.hidden = false;
+		overlay.style.display = 'block';
 		startX = -1, startY = -1;
+		self.addEventListener('wheel', onWheel, { passive: false });
+		self.addEventListener('mouseup', onMouseUp, { passive: true });
+		self.addEventListener('contextmenu', preventDefault, { once: true });
+	}],
+	['leave', () => {
+		overlay.hidden = true;
+		overlay.style.display = 'none';
+		self.removeEventListener('mousemove', checkState);
+		self.removeEventListener('wheel', onWheel);
+		self.removeEventListener('mouseup', onMouseUp);
+		self.removeEventListener('contextmenu', preventDefault);
+	}],
+]);
+port.onMessage.addListener(/** @param m { { func?:string, error?:string } } */ m => {
+	if (m.func && funcMap.has(m.func)) {
+		funcMap.get(m.func)();
+	} else if (m.error) {
+		throw m.error;
 	}
-	
-	function checkstate(e) {
-		const diffX = e.screenX - startX, diffY = e.screenY - startY;
-		const absX = Math.abs(diffX), absY = Math.abs(diffY);
-		const min = first ? vars.mm1 : vars.mm2;
-
-		if (min < absX || min < absY) {			
-			let states;
-			if (absX > absY) {
-				if (vars.tan > absY / absX) {
-					states = [diffX < 0 ? 'left' : 'right'];
-				} else {
-					states = [null, diffX < 0 ? 'left' : 'right'];
-				}
-			} else if (absY > absX) {
-				if (vars.tan > absX / absY) {
-					states = [diffY < 0 ? 'up' : 'down'];
-				} else {
-					states = [null, diffY < 0 ? 'up' : 'down']
-				}
-			}
-			if (states) {
-				first = false;
-				startX = e.screenX, startY = e.screenY;
-				port.postMessage({ states: states });
-			}
-		}
-	}
-	function onwheel(e) {
-		e.preventDefault();
-		clearTimeout(wheelTimer);
-		if (startX < 0 || startY < 0) {
-			startX = e.screenX, startY = e.screenY;
-		}
-		startX += e.deltaX, startY += e.deltaY;
-		const diffY = startY - e.screenY;
-		if (vars.wm < Math.abs(diffY)) {
-			startX = e.screenX, startY = e.screenY;
-			port.postMessage({ type: 'wheel', direction: Math.sign(diffY) });
-		}
-		wheelTimer = setTimeout(initStartCoord, 100);
-	}
-	function onmouseup(e) {
-		if (e.button === 2) {
-			overlay.hidden = true;
-			overlay.style.display = 'none';
-			port.postMessage({ execute: true, url: href });
-			self.removeEventListener('mousemove', checkstate);
-			self.removeEventListener('wheel', onwheel);
-			self.removeEventListener('mouseup', onmouseup);
-		}
-	}
-
-	self.addEventListener('message', e => {
-		if (e.data.origin === EXT_URL) {
-			overlay.hidden = false;
-			overlay.style.display = 'block';
-			first = true;
-			startX = e.data.screenX, startY = e.data.screenY;
-			href = e.data.href;
-			self.addEventListener('mousemove', checkstate, { passive: true });
-			self.addEventListener('wheel', onwheel, { passive: false });
-			self.addEventListener('mouseup', onmouseup, { passive: true });
-		}
-	}, false);
-	
-	const func = new Map([
-		['left', () => { history.back(); }],
-		['right', () => { history.forward(); }],
-		['up', () => { window.stop(); }],
-		['up,left', () => { location.href = location.href.replace(/[^/]*$/, ''); }],
-		['up,down', () => { location.reload(); }],
-		['enter', () => {
-			overlay.hidden = false;
-			overlay.style.display = 'block';
-			startX = -1, startY = -1;
-			self.addEventListener('wheel', onwheel, { passive: false });
-			self.addEventListener('mouseup', onmouseup, { passive: true });
-			self.addEventListener('contextmenu', preventDefault, { once: true });
-		}],
-		['leave', () => {
-			overlay.hidden = true;
-			overlay.style.display = 'none';
-			self.removeEventListener('mousemove', checkstate);
-			self.removeEventListener('wheel', onwheel);
-			self.removeEventListener('mouseup', onmouseup);
-			self.removeEventListener('contextmenu', preventDefault);
-		}],
-	]);
-	port.onMessage.addListener(m => {
-		if (m.func && func.has(m.func)) {
-			func.get(m.func)();
-		} else if (m.error) {
-			throw m.error;
-		}
-	});
-})();
+});
